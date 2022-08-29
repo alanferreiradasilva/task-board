98f30f2ecf7788ae4509f5a9c7f660552284c4c0
Alan <alan@alephcrm.com>
2022-08-28 21:07:25 -0300

    fix https port number.

--- FRONT/src/environments/environment.ts
+++ FRONT/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:5000/',
-  apiUrl: 'https://localhost:44333/',
+  apiUrl: 'https://localhost:7001/',
   apiCredencials: { "login": "letscode", "senha": "lets@123" },
 };
